5ea86dfee6e5f240e6f2834f7b91e482bff387c9
siddharth-bhatnagar <siddharth.bhatnagar.msit@iosd.tech>
2021-03-25 18:50:35 +0530

    updates

---
+++ src/app.js
@@ -0,0 +1,9 @@
+// Register service worker
+if ('serviceWorker' in navigator) {
+    window.addEventListener('load', () => {
+        navigator.serviceWorker
+            .register('../serviceWorker.js')
+            .then((registration) => console.log(`Registration Successful, scope: ${registration.scope}`))
+            .catch((error) => console.log(`Service Worker Registration failed: ${error}`));
+    });
+}
