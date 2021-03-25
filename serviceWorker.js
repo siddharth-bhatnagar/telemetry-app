867172c0d32e2c5e2f525ea2c3c0b5188e4cc78f
siddharth-bhatnagar <siddharth.bhatnagar.msit@iosd.tech>
2021-03-25 18:16:22 +0530

    added service worker

---
+++ serviceWorker.js
@@ -0,0 +1,45 @@
+// Define CACHE
+const CACHE_NAME = "version-1";
+
+// Assets to be Cached
+const cacheAssets = ["index.html", "offline.html", "src/data.html", "src/diagnosticsManager.html", "src/state.html", "src/app.js"];
+
+const self = this;
+
+// Install SW
+self.addEventListener("install", (event) => {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then((cache) => {
+                console.log("Opened Cache");
+                return cache.addAll(cacheAssets);
+            })
+    )
+});
+
+// Listen for Requests
+self.addEventListener("fetch", (event) => {
+    event.respondWith(
+        caches.match(event.request)
+            .then(() => {
+                return fetch(event.request)
+                .catch(() => caches.match("offline.html"))
+            })
+    )
+});
+
+// Activate the SW
+self.addEventListener("activate", (event) => {
+    const cacheWhiteList = [];
+    cacheWhiteList.push(CACHE_NAME);
+
+    event.waitUntil(
+        caches.keys().then((cacheNames) => Promise.all(
+            cacheNames.map((cacheName) => {
+                if(!cacheWhiteList.includes(cacheName)){
+                    return caches.delete(cacheName);
+                }
+            })
+        ))
+    )
+});
